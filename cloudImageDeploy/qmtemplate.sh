f6afb3f56caea80f8feb6d4a04850f0270456e02
Itarun Pitimon[xOps] <city@en.rmutt.ac.th>
2024-05-22 11:09:17 +0700

    0522

---
+++ cloudImageDeploy/qmtemplate.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+VMID=$1
+bridge=$2
+imagename=$3
+storage=$4
+qm create $VMID --core 2 --memory 2048 --name Cloud$VMID --net0 virtio,bridge=$2 --serial0 socket --agent enabled=1
+qm disk import $VMID $imagename $storage
+qm set $VMID --scsihw virtio-scsi-pci --scsi0 $storage:vm-$VMID-disk-0
+qm resize $VMID scsi0 +1G
+qm set $VMID --ide2 $storage:cloudinit
+qm set $VMID --boot c --bootdisk scsi0
+qm set $VMID --ipconfig0 ip=dhcp
